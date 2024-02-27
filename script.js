a62646efe615bce8e2248861ef9fb05af78c694b
SudheshnaRamanchi <160815654+SudheshnaRamanchi@users.noreply.github.com>
2024-02-27 17:51:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+alert("Well come to the applation page"); 
+
+console.log("hello everyone");
+
+//varibles
+
+let number1 =20
+let number2=25
+let number3=30
+console.log(number1);
+console.log("number2");
+console.log(number1+number3);
+
+
+//data types
+ 
+let data =27
+let month = "fedruary"
+let time =17.34
+let bool =true
+console.log(typeof(month));
+console.log(typeof(time));
+console.log(typeof(bool));
+
+
+
+
+
+
+
